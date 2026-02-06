3b8565ef36a5a58452187be213ade1646b000d11
keremozt <keremoztopuzzz@gmail.com>
2026-02-06 18:04:35 +0300

    add sigmoid multilabel prediction

--- src/predict.py
+++ src/predict.py
@@ -1,13 +1,12 @@
 import os
 import torch
 from PIL import Image
-import torch.nn.functional as F
 from torchvision import transforms
 
-from config import DEVICE, MODEL_SAVE_PATH, CLASS_NAMES, DATA_DIR, IMG_SIZE, MEAN, STD
+from config import DEVICE, MODEL_SAVE_PATH, CLASS_NAMES, DATA_DIR, IMG_SIZE, MEAN, STD, DETECTION_THRESHOLD
 from model import build_model
 
-TEST_IMAGE_PATH = os.path.join(DATA_DIR, "test", "acne", "acne-cystic-97.jpg")
+TEST_IMAGE_PATH = os.path.join(DATA_DIR, "test", "acne", "acne-cystic-144.jpg")
 
 def get_transform():
     return transforms.Compose([
@@ -28,14 +27,17 @@ def predict(image_path):
 
     with torch.no_grad(): 
         outputs = model(input_tensor)
-        probabilities = F.softmax(outputs, dim=1)
+        probabilities = torch.sigmoid(outputs)
+        probs = probabilities[0].tolist()
+        detected = {name: prob for name, prob in zip(CLASS_NAMES, probs) if prob > DETECTION_THRESHOLD}
         confidence, predicted_idx = torch.max(probabilities, 1)
         predicted_class = CLASS_NAMES[predicted_idx.item()]
 
         return {
             "Class": predicted_class,
             "Confidence": confidence.item(),
-            "Probabilities": dict(zip(CLASS_NAMES, probabilities[0].tolist()))
+            "Probabilities": dict(zip(CLASS_NAMES, probs)),
+            "Detected": detected
         }
 
 if __name__ == "__main__":
