fb8bc84ab7129833dda4908fdbf548afff7d33bd
Berat Kerem Öztopuz <101475726+keremoztopuz@users.noreply.github.com>
2026-08-22 18:18:34 +0300

    Add staged backbone freezing to model factory

--- src/model.py
+++ src/model.py
@@ -5,13 +5,35 @@ def build_model(model_name=None, num_classes=None, pretrained=True):
     model_name = model_name or config.MODEL_NAME
     num_classes = num_classes or config.NUM_CLASSES
 
-    model = timm.create_model(model_name,
-    num_classes=num_classes,
-    pretrained=pretrained,
-    drop_rate=config.DROP_RATE,
-    drop_path_rate=config.DROP_PATH_RATE
+    model = timm.create_model(
+        model_name,
+        num_classes=num_classes,
+        pretrained=pretrained,
+        drop_rate=config.DROP_RATE,
+        drop_path_rate=config.DROP_PATH_RATE
     )
     
+    return model
+
+def freeze_backbone(model, unfreeze_last_n_stages=None):
+    """Freeze the backbone, then unfreeze the final ConvNeXt stages and head."""
+    unfreeze_last_n_stages = (
+        config.UNFREEZE_LAST_N_STAGES
+        if unfreeze_last_n_stages is None
+        else unfreeze_last_n_stages
+    )
+
+    for param in model.parameters():
+        param.requires_grad = False
+
+    if unfreeze_last_n_stages > 0:
+        for stage in model.stages[-unfreeze_last_n_stages:]:
+            for param in stage.parameters():
+                param.requires_grad = True
+
+    for param in model.head.parameters():
+        param.requires_grad = True
+
     return model
 
 def get_model_info(model):
